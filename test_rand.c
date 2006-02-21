c69936abbfaa1dc50436c48dee2c7d80175317b0
ashted <ashted@6fd91924-e447-0410-ba6f-9c974466b4e1>
2006-02-21 20:36:05 +0000

    Testing random vector generator.

--- test_rand.c
+++ test_rand.c
@@ -1,14 +1,26 @@
 #include "plCurve.h"
+#include "stdio.h"
 
 plcl_vector plcl_random_2();
 
+#define POINTS 1000
 int main () {
   int i;
   plcl_vector R;
 
-  for (i = 0; i < 1000000; i++) {
-    R = plcl_random_vect();
+  printf("VECT\n1000 2000 0\n");
+  for (i = 0; i < 1000; i++) {
+    printf("2 ");
+  }
+  printf("\n");
+  for (i = 0; i < 1000; i++) {
+    printf("0 ");
+  }
+  printf("\n");
+  for (i = 0; i < 1000; i++) {
     R = plcl_random_2();
+    printf("%lg %lg %lg\n",plcl_M_clist(0.9*R));
+    printf("%lg %lg %lg\n",plcl_M_clist(R));
   }
 
   return 0;
