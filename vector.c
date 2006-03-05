88b82b45d6f34987c1b7c34fd7d8d780505b847f
ashted <ashted@6fd91924-e447-0410-ba6f-9c974466b4e1>
2006-03-05 16:20:33 +0000

    Trying to get code to compile and yet preserve dladdr when possible.

--- vector.c
+++ vector.c
@@ -3,7 +3,7 @@
  *
  * Routines for working with vectors.
  *
- * $Id: vector.c,v 1.30 2006-03-03 22:51:52 ashted Exp $
+ * $Id: vector.c,v 1.31 2006-03-05 16:20:33 ashted Exp $
  *
  */
 
@@ -45,7 +45,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #ifdef HAVE_ASSERT_H
   #include <assert.h>
 #endif
-#if (defined(HAVE_DLFCN_H)) && (!defined(S_SPLINT_S))
+#if (defined(HAVE_DLFCN_H)) && defined(HAVE_DLADDR) && (!defined(S_SPLINT_S))
   #include <dlfcn.h>
 #endif
 
@@ -87,7 +87,7 @@ inline plcl_vector plcl_component_mult(plcl_vector A,plcl_vector B) {
 /* Should we add an "ok" parameter here as in _normalize_vect? */
 inline plcl_vector plcl_component_div(plcl_vector A,plcl_vector B,
                                       /*@null@*/ bool *ok) {
-#if (defined(HAVE_DLFCN_H)) && (!defined(S_SPLINT_S))
+#if (defined(HAVE_DLFCN_H)) && defined(HAVE_DLADDR) && (!defined(S_SPLINT_S))
   Dl_info dli;
 #endif
 
@@ -104,7 +104,7 @@ inline plcl_vector plcl_component_div(plcl_vector A,plcl_vector B,
       plcl_M_component_div(A,B);
     } else {
       fprintf(stderr,"plcl_component_div: Divisor has zero component.\n");
-#if (defined(HAVE_DLFCN_H)) && (!defined(S_SPLINT_S))
+#if (defined(HAVE_DLFCN_H)) && defined(HAVE_DLADDR) && (!defined(S_SPLINT_S))
       dladdr(__builtin_return_address(0), &dli);
       fprintf(stderr,"plcl_component_div: called from: %s\n",dli.dli_fname);
 #endif
@@ -142,7 +142,7 @@ inline bool plcl_vecteq(plcl_vector A, plcl_vector B) /*@modifies nothing@*/ {
 inline plcl_vector plcl_normalize_vect(const plcl_vector V,
                                        /*@null@*/ bool *ok) {
   double vnrm;
-#if (defined(HAVE_DLFCN_H)) && (!defined(S_SPLINT_S))
+#if (defined(HAVE_DLFCN_H)) && defined(HAVE_DLADDR) && (!defined(S_SPLINT_S))
   Dl_info dli;
 #endif
 
@@ -153,7 +153,7 @@ inline plcl_vector plcl_normalize_vect(const plcl_vector V,
     } else {
       fprintf(stderr,
         "plcl_normalize_vect: Attempted to normalize zero vector.\n");
-#if (defined(HAVE_DLFCN_H)) && (!defined(S_SPLINT_S))
+#if (defined(HAVE_DLFCN_H)) && defined(HAVE_DLADDR) && (!defined(S_SPLINT_S))
       dladdr(__builtin_return_address(0), &dli);
       fprintf(stderr,"plcl_normalize_vect: called from: %s\n",dli.dli_fname);
 #endif
