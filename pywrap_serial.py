09d4872d23fa1eb5de6bbfe4f8655918a2466f28
Harrison Chapman <hchaps@gmail.com>
2013-08-23 17:41:12 +0000

    remove serialization python silly stub

--- pywrap_serial.py
+++
@@ -1,5 +0,0 @@
-import libplcurve.plcurve as pl
-r = pl.RandomGenerator()
-p = pl.PlCurve.random_closed_polygon(r, 44)
-serial = p.serialize()
-print(":".join("{0:02x}".format(ord(c)) for c in serial))
