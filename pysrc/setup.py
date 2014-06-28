904a48499448a9dcd528f05b8068dc934b3aeea4
Harrison Chapman <hchaps@gmail.com>
2014-06-28 22:03:51 +0000

    forgot trailing /core/include

--- pysrc/setup.py
+++ pysrc/setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
+import os
 
 extensions = [
     Extension("*", ["libpl/*.pyx"],
-              include_dirs = ["../src",".",numpy.__path__[0]],
+              include_dirs = ["../src",".",os.path.join(numpy.__path__[0], "core", "include")],
               library_dirs = ["../src/.libs"],
               libraries = ["gsl","plCurve"]),
 #    Extension("pdcode", ["libpl/pdcode.pyx"],
