1fe716e6443cd636155da22d1abc48a5f1093493
Harrison Chapman <hchaps@gmail.com>
2014-07-18 15:21:04 +0000

    added tests for pdcode python-specific features (only pickling for now)

---
+++ pysrc/test/test_pdcode_python.py
@@ -0,0 +1,18 @@
+import unittest
+from itertools import product, compress, izip
+from libpl.pdcode import PlanarDiagram
+
+class TestPDCode(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_pickle(self):
+        import cPickle
+        pdc = PlanarDiagram.from_torus_knot(2, 11)
+        pdc2 = PlanarDiagram.from_torus_knot(2, 7)
+        pdc_new = cPickle.loads(cPickle.dumps(pdc))
+        self.assertTrue(pdc.isomorphic(pdc_new))
+        self.assertFalse(pdc2.isomorphic(pdc_new))
+
+if __name__=="__main__":
+    unittest.main()
