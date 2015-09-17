f1ecc4732ca5f98968da30601f4556381aa253f2
Jason Cantarella <jason.cantarella@gmail.com>
2015-09-17 22:37:14 +0000

    Ok, the classifyInstance script seems to run correctly. Now for the controller!

--- tools/classifyInstance.py
+++ tools/classifyInstance.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import argparse
 import tempfile
 import os
@@ -5,7 +6,8 @@ import subprocess
 import re
 import sys
 import atexit
-
+import time
+import shutil
 
 def garbagecollection(tdir):
     print "<in a real execution, would delete " + tdir + " now>"
@@ -71,6 +73,7 @@ if __name__ == "__main__":
         size = os.stat("./" + inputpd).st_size
     except:
         print "FAIL (couldn't stat ./" + inputpd
+        sys.exit(1)
 
     print "done (" + humansize(size) + ") input file ready"
 
@@ -88,11 +91,111 @@ if __name__ == "__main__":
         print "\toutput: " + e.output
         sys.exit(1)
 
-    print "done"
-    print "cI: pdclassify output\n" + classifyOutput
+    ambigFile = inputpd + ".ambig.pdstor"
+    countFile = inputpd + ".counts.tsv"
+        
+    try:
+        ambigSize = os.stat(ambigFile).st_size
+    except:
+        print "FAIL (couldn't stat " + ambigFile
+        sys.exit(1)
 
+    try:
+        countsSize = os.stat(countFile).st_size
+    except:
+        print "FAIL (couldn't stat " + countFile
+        sys.exit(1)
+        
+    print "done (produced "+humansize(ambigSize)+" file of ambiguous knots, "+humansize(countsSize)+" file of counts)"
 
+    # task 4 is to push the results into the s3 bucket
+
+    print "cI: pushing results to s3 bucket knotclassification ...",
+
+    basename = re.match("./(.+).pdstor",inputpd).group(1);
+    n = re.match("./(\d+)b-.+.pdstor",inputpd).group(1);
+
+    try:
+        awsOutput = subprocess.check_output("aws s3 cp " + ambigFile + " s3://knotclassification/outputs/" + n + "/ambigs/" + basename + ".ambig.pdstor",
+                                            stderr=subprocess.STDOUT,
+                                            shell=True)
+    except subprocess.CalledProcessError as e:
+        print "FAIL (s3 error, probably couldn't find file to upload)"
+        print "\tinput: " + e.cmd
+        print "\toutput: " + e.output
+        sys.exit(1)
+
+    try:
+        awsOutput = subprocess.check_output("aws s3 cp " + countFile + " s3://knotclassification/outputs/" + n + "/counts/" + basename + ".counts.tsv",
+                                            stderr=subprocess.STDOUT,
+                                            shell=True)
+    except subprocess.CalledProcessError as e:
+        print "FAIL (s3 error, probably couldn't find file to upload)"
+        print "\tinput: " + e.cmd
+        print "\toutput: " + e.output
+        sys.exit(1)
+        
+    time.sleep(2) # wait 2 seconds for aws to realize that the upload is complete.
+
+    # task 5 is to confirm that the upload actually happened
     
+    try:
+        awsOutput = subprocess.check_output("aws s3 ls s3://knotclassification/outputs/" + n + "/ambigs/"+ basename + ".ambig.pdstor",
+                                            stderr=subprocess.STDOUT,
+                                            shell=True)
+        
+    except subprocess.CalledProcessError as e:
+        print "FAIL (s3 error, probably couldn't find file to uploadbucket)"
+        print "\tinput: " + e.cmd
+        print "\toutput: " + e.output
+        sys.exit(1)
+
+    match = re.match('.+ (\d+) '+ basename +'.ambig.pdstor',awsOutput)
+
+    if (match == None) :
+        print "FAIL (s3 couldn't confirm the file was uploaded)"
+        print "couldn't match" + '.+ (\d)+ '+ basename +'.ambig.pdstor'
+        print "awsOutput: " + awsOutput
+        sys.exit(1)
+
+    ambigCheckSize = int(match.group(1))
+
+    if (int(ambigCheckSize) != int(ambigSize)):
+        print "WARN (s3 uploaded size " + ambigCheckSize + " != local filesize " + ambigSize
+        print "cI: This could be a size printing convention problem for large files"
+
+    try:
+        awsOutput = subprocess.check_output("aws s3 ls s3://knotclassification/outputs/" + n + "/counts/"+ basename + ".counts.tsv",
+                                            stderr=subprocess.STDOUT,
+                                            shell=True)
+        
+    except subprocess.CalledProcessError as e:
+        print "FAIL (s3 error, probably couldn't find file to uploadbucket)"
+        print "\tinput: " + e.cmd
+        print "\toutput: " + e.output
+        sys.exit(1)
+
+    match = re.match('.+ (\d+) '+ basename +'.counts.tsv',awsOutput)
+
+    if (match == None) :
+        print "FAIL (s3 couldn't confirm the file was uploaded)"
+        print "couldn't match" + '.+ (\d)+ '+ basename +'.ambig.pdstor'
+        print "awsOutput: " + awsOutput
+        sys.exit(1)
+
+    countsCheckSize = int(match.group(1))
+
+    if (int(countsCheckSize) != int(countsSize)):
+        print "WARN (s3 uploaded size " + countsCheckSize + " != local filesize " + countsSize
+        print "cI: This could be a size printing convention problem for large files"
+
+        
+    print "done (checked "+humansize(ambigCheckSize)+" file of ambigs uploaded,"+humansize(countsCheckSize)+" file of counts uploaded)"
+
+    # task 6 is garbage collection (delete the temp directory)
+
+    shutil.rmtree(tempdir)
     
+    # task 7, if we were called with the --shutdown-on-completion flag, is to kill this machine
     
     
