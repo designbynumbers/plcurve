09c480ff4b8716647c0b45c80dc7bbe0d807bdcf
Jason Cantarella <jason.cantarella@gmail.com>
2015-09-29 15:22:58 +0000

    hacked getopt to remove libintl reference

--- src/getopt.c
+++ src/getopt.c
@@ -94,13 +94,13 @@
 #ifndef _
 /* This is for other GNU distributions with internationalized messages.
    When compiling libc, the _ macro is predefined.  */
-#ifdef HAVE_LIBINTL_H
-#include <libintl.h>
-#define _(msgid)	gettext (msgid)
-#else
+//#ifdef HAVE_LIBINTL_H
+//#include <libintl.h>
+//#define _(msgid)	gettext (msgid)
+//#else
 #define _(msgid)	(msgid)
 #endif
-#endif
+//#endif
 
 /* This version of `getopt' appears to the caller like standard Unix `getopt'
    but it behaves differently for the user, since it allows the user
