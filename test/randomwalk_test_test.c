1722a1309c610c82de1bc09f3fdfa937b4fe0a1a
Harrison Chapman <hchaps@gmail.com>
2016-03-22 18:25:15 +0000

    document randomwalk_test_test

--- test/randomwalk_test_test.c
+++ test/randomwalk_test_test.c
@@ -1,3 +1,27 @@
+/* randomwalk_test_test.c
+author: Harrison Chapman
+
+This is a file which tests `randomwalk_test()`, which is a part of the
+ccode_test test suite.
+
+We know that there will be some cases in which `randomwalk_test()`
+will fail to project to a pdcode object; principle among reasons are
+those in which edges of the random polygon generated are
+self-intersecting (in which case we would be unable to determine the
+sign of that given crossing in constructing the pdcode object).
+
+Based on a prior run of this program, the expected success rate of a
+given loop run in randomwalk_test() run on random polygons with 250
+edges is about 999974 in 1000000 (99.9974%). Hence, the expected
+failure probability of a given random 250-gon is 2.6e-5.
+
+In ccode_test, randomwalk_test() is called to process 1001 random
+250-gons into pdcode objects. If a single loop 'fails', the entire
+test 'fails'. Based on the prior run of this program, we estimate (as
+of March 2016) that this step should fail about 97.431% of the time,
+or about 3-in-100 times.
+*/
+
 #include "config.h"
 #include "plCurve.h"
 #include "plcTopology.h"
@@ -60,10 +84,10 @@ int randomwalk_test_instance(gsl_rng *rng, int nedges, bool verbose) {
         printf("-----------------------------------\n");
         pd_write(stdout,projected_pd);
         pd_code_free(&projected_pd);
-        
+
         return -1;
     }
-    
+
     pd_code_free(&projected_pd);
     plc_free(L);
     return ncross;
@@ -83,14 +107,14 @@ bool randomwalk_test(gsl_rng *rng,int nedges,bool verbose,int reps) {
   set_pd_code_from_plCurve_verbose(verbose);
 
   if (verbose) { printf("\n\n"); }
-  
+
   int i;
   int n_success = 0;
   double totalcross = 0;
   double ncross;
   for(i=0;i<reps;i++) {
       ncross = randomwalk_test_instance(rng, nedges, verbose);
-      
+
       if (ncross < 0) {
           // test failure
       } else {
@@ -99,14 +123,14 @@ bool randomwalk_test(gsl_rng *rng,int nedges,bool verbose,int reps) {
           totalcross += ncross;
       }
   }
-  
+
   end = clock();
   cpu_time_used = ((double)(end - start)/CLOCKS_PER_SEC);
   printf("done (%2.4g sec, %i of %i (=%f) passed pd_ok)\n",
          cpu_time_used, n_success, reps, (1.0*n_success)/(1.0*reps));
 
   printf("average #crossings of produced pd codes...%3.4g\n",totalcross/(double)(reps));
-  
+
   return true;
- 
+
 }
